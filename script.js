7c0d24de8ba5f3488656093b102c66f97ce2d2d3
Juan Mireles <jmireles12@gmail.com>
2020-07-23 00:56:08 -0500

    something

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer 6ac81b7d5b55b3648e27f614a3ae07d4e6cdc544");
+myHeaders.append("Authorization", "Bearer 425652b2cf1bde8f0ab72c3bd020e40303c016a2");
 
 var requestOptions = {
   method: 'GET',
@@ -8,28 +7,29 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-function getRepos(username) {
-    fetch(`https://api.github.com/users/${username}/repos?type=owner&sort=full_name&direction=desc`, requestOptions)
-  .then(response => response.text())
-  .then(result => displayResult(result))
+function getRepos(user) {
+    fetch(`https://api.github.com/users/${user}/repos`, requestOptions)
+  .then(response => response.json())
+  .then(responseJson => displayResult(responseJson))
   .catch(error => alert('error'));
+  console.log(responseJson)
 };
 
-function displayResult(result) {
-    for (let i = 0; i < result.length; i++) {
-        console.log(result);
-        $('#result-list').append(`<li class=repo-list>${responseJson.full_name}</li>`)
-        $('.result').removeClass('hidden');
-    }
+function displayResult(responseJson) {
+    //for (let i = 0; i < result.length; i++) {
+        console.log(responseJson);
+        $('#result-list').append(`<li>${responseJson.message}</li>`);
+        $('#result').removeClass('hidden');
+    //}
 
 }
 
 function watchForm() {
     $('form').submit(event => {
-        event.preventDefault();
-        let name = $('#js-search').val()
+        let name = $(user).val();
         console.log('submit');
-        getRepos(name)
+        event.preventDefault();
+        getRepos(name);
     });
 }
 
