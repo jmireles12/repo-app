adb1d39aec9082db9f698db37ef0714885a74a6f
Juan Mireles <jmireles12@gmail.com>
2020-07-23 22:42:55 -0500

    display repo list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer 425652b2cf1bde8f0ab72c3bd020e40303c016a2");
+myHeaders.append("Authorization", "Bearer aa3d1f6acc68f7242af0a9b193c0b15f9bd8ada3");
 
 var requestOptions = {
   method: 'GET',
@@ -8,28 +8,29 @@ var requestOptions = {
 };
 
 function getRepos(user) {
-    fetch(`https://api.github.com/users/${user}/repos`, requestOptions)
-  .then(response => response.json())
-  .then(responseJson => displayResult(responseJson))
-  .catch(error => alert('error'));
-  console.log(responseJson)
+    fetch(`https://api.github.com/users/${user}/repos?type=owner&sort=full_name&direction=desc`, requestOptions)
+    .then(response => response.json())
+    .then(responseJson => displayResult(responseJson))
+    .catch(error => alert('Network error'));
 };
 
 function displayResult(responseJson) {
-    //for (let i = 0; i < result.length; i++) {
+    for (let i = 0; i < responseJson.length; i++) {
         console.log(responseJson);
-        $('#result-list').append(`<li>${responseJson.message}</li>`);
+        $('#result-list').append(`<li>Repo name:${responseJson[i].name}<br> 
+        Repo url: <a href="https://github.com/${responseJson[i].full_name}.git">https://github.com/${responseJson[i].full_name}.git</a></li>`);
         $('#result').removeClass('hidden');
-    //}
+    }
 
 }
 
 function watchForm() {
     $('form').submit(event => {
-        let name = $(user).val();
+        let name = $('input[type="search"]').val();
         console.log('submit');
         event.preventDefault();
         getRepos(name);
+        $('#result-list').html('');
     });
 }
 
